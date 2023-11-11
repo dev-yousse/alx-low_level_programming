b6d84ab93451004fb5d1e67a7dfa1f8d8414d6c5
dev-yousse <youssef@gmail.com>
2023-11-11 01:03:30 -0800

    wdd

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,24 @@
-#include "variadic_functions.h"
+#include "variadic_function.h"
+
+
 /**
- * sum_them_all - sum all parameter passed to the function
- * @n: number of argument to be passed to be passed to sun
- * Return: sum
- */
+* sum_them_all - adds all the numbers
+* @n: the number of parameters passed
+* Return: Always 0
+*/
+
 
 int sum_them_all(const unsigned int n, ...)
+{
 
-	{
-		usigned int sum = 0;
-		usigned int i;
-
-		va_list holder;
-
-                va_start (holder, n);
-
-                 for (i = 0; i < n; i++ )
-			 {
-			 if (n = 0)
-			 { return (0);
-			 }
-	                  else 
-
-	}
+	int sum;
+	unsigned int i;
+	va_list ap;
+	va_start(ap, n);
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
